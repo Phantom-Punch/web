64f25f20f8eb2e6fc92616ea47c83e30fa0d82bd
Mythreya <lmythreya@gmail.com>
2017-03-15 07:51:45 +0530

    modified:   a.py 	modified:   data.csv 	new file:   static/data.csv 	modified:   templates/index.html

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import os
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, Response
 from flask import request
 from flask import send_from_directory
 app = Flask(__name__)
@@ -16,19 +16,22 @@ def user(string):
     roll = string[0:6]
     credits = string[6:8]
     courses = string[8:27]
-    email = string[27:]
+    name = string[27:]
     time = os.popen('date').read()
-    output_string = time[:-1] + " , " + roll + " , " + credits + " , " + email
+    output_string = time[:-1] + " , " + roll + " , " + credits + " , " + name 
     for i in range(len(courses)):
         output_string = output_string + " , " + courses[i]
-    file = open("data.csv", "a")
+    file = open("static/data.csv", "a")
     file.write("%s\n" % output_string)
     file.close()
-    return '<h1>Your preferences have been submitted %s!</h1>' % roll
+    return '<h1>Your preferences have been submitted %s!</h1>' % name 
 
-@app.route('/viewdata')
-def viewdata():
-    return os.popen("cat data.csv").read()
+
+#@app.route('/viewdata')
+#def viewdata():
+#    os.system("cp data.csv static/data.csv")
+#    return app.send_static_file('static/data.csv')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
